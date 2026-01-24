2509f306d7363ae450c513dd5843213fad89ead4
Moris L. Porteza <morisporteza60@gmail.com>
2026-01-24 09:35:44 +0800

    Update AbstractTask.java

--- src/model/AbstractTask.java
+++ src/model/AbstractTask.java
@@ -1,29 +1,48 @@
 package model;
 
 public abstract class AbstractTask {
+
     protected int taskId;
     protected String taskName;
     protected String taskDescription;
 
-    public AbstractTask() {}
+    // Default Constructor
+    public AbstractTask() {
+    }
 
+    // Parameterized Constructor
     public AbstractTask(int taskId, String taskName, String taskDescription) {
         this.taskId = taskId;
         this.taskName = taskName;
         this.taskDescription = taskDescription;
     }
 
-    public int getTaskId() { return taskId; }
-    public void setTaskId(int taskId) { this.taskId = taskId; }
+    // Abstract Getter and Setter for Status
+    public abstract String getStatus();
+    public abstract void setStatus(String status);
 
-    public String getTaskName() { return taskName; }
-    public void setTaskName(String taskName) { this.taskName = taskName; }
+    // Normal Getters and Setters
+    public int getTaskId() {
+        return taskId;
+    }
 
-    public String getTaskDescription() { return taskDescription; }
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
     public void setTaskDescription(String taskDescription) {
         this.taskDescription = taskDescription;
     }
-
-    public abstract String getStatus();
-    public abstract void setStatus(String status);
 }
